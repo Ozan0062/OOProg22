4e8633ada912382a40b2bc37fd2f4c9d22c900dc
Ozan0062 <ozan0062@edu.zealand.dk>
2022-09-26 12:15:48 +0200

    RolePlay

--- Chap/OOProg01/Unsolved/RolePlayV10/Program.cs
+++ Chap/OOProg01/Unsolved/RolePlayV10/Program.cs
@@ -1,6 +1,16 @@
 ﻿
+// Constructors
 Warrior warriorA = new Warrior("Ragnar");
 Warrior warriorB = new Warrior("Lagertha");
 
-Console.WriteLine($"Warrior A is called {warriorA.Name}");
-Console.WriteLine($"Warrior B is called {warriorB.Name}");
+Console.WriteLine($"Warrior A is called {warriorA.Name}," +
+    $"and is level {warriorA.Level}");
+Console.WriteLine($"Warrior B is called {warriorB.Name}," +
+    $"and is level {warriorB.Level}");
+
+warriorA.UpgradeLevel();
+
+Console.WriteLine($"Warrior A is called {warriorA.Name}," +
+    $"and is level {warriorA.Level}");
+Console.WriteLine($"Warrior B is called {warriorB.Name}," +
+    $"and is level {warriorB.Level}");
